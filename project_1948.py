40b9386ef8e8070b7bc98f0b05d45143be4b8c84
JMGC34 <32426806+JMGC34@users.noreply.github.com>
2017-10-31 12:12:49 -0400

    Add files via upload

--- project_1948.py
+++ project_1948.py
@@ -1,5 +1,7 @@
 import re
 import string
+import numpy as np
+import pandas as pd
 from docx import Document
 
 #Prepare function to read in documents
@@ -45,14 +47,20 @@ def normalize_text(input_text,upper=False,lower=False,title=False):
     words = input_text.split()
     output_text=""
     if upper:
+        j=0
         for word in words:
-            word.upper()
+            words[j] = word.upper()
+            j=j+1
     if lower:
+        j=0
         for word in words:
-            word.lower()
+            words[j] = word.lower()
+            j=j+1
     if title:
+        j=0
         for word in words:
-            word.title()
+            words[j] = word.title()
+            j=j+1
     output_text=string.join(words)
     return output_text
         
@@ -108,3 +116,49 @@ def word_context(input_text, word_list, radius):
     else:
         print("ERROR IN TYPE")
     return output
+
+#function to break up test string by sentences
+    #input_text is text to be split
+def sentences(input_text):
+    new_text = string.replace(input_text,'?','.')
+    new_text = string.replace(new_text,'!','.')
+    sentence_list = []
+    index_end = new_text.find('. ')
+    while index_end>=0:
+        sentence_list.append(new_text[:index_end+1])
+        new_text = new_text[index_end+1:]
+        index_end = new_text.find('. ')
+    if new_text not in string.whitespace:
+        sentence_list.append(new_text)
+    return sentence_list
+    
+#function to count how many times pairs of words occur in the same sentence
+#entry i,j of returned data frame is number of sentences containing word i and word j 
+#entry i,i of returned data frame is number of sentences contaiing word i
+    #sentence_list is a list containing the sentences of the text
+    #words is a list containing the specific words being correlated.
+def word_pairs(sentence_list, words):
+    np_zeros = np.zeros(shape=(len(words),len(words)))
+    word_pairs = pd.DataFrame(data=np_zeros, columns = words, index = words)
+    for outer_word in words:
+        for sentence in sentence_list:
+            if outer_word in sentence:
+                for inner_word in words:
+                    if inner_word in sentence:
+                        old_count = word_pairs.get_value(outer_word,inner_word)
+                        word_pairs.set_value(outer_word,inner_word,old_count+1)
+    return word_pairs
+
+#function to determine how many sentences contain a set list of words
+    #sentence_list is a list containing sentences of text
+    #words is a list of the words being searched for
+def words_in_sentences(sentence_list, words):
+    count_main = 0
+    for sentence in sentence_list:
+        j=0
+        for word in words:
+            if word in sentence:
+                j=j+1
+        if j==len(words):
+            count_main = count_main+1
+    return count_main
